e275b28e9f5caa387eb581509c39dee171199e60
Nils Winter <nils.winter@yahoo.com>
2021-03-18 23:02:38 +0100

    use urh backend directly because device init is too slow

---
+++ sdr.py
@@ -0,0 +1,173 @@
+#!/usr/bin/python3
+
+import argparse
+import logging
+import os
+import sys
+import time
+
+import numpy as np
+
+cli_exe = sys.executable if hasattr(sys, 'frozen') else sys.argv[0]
+cur_dir = os.path.realpath(os.path.dirname(os.path.realpath(cli_exe)))
+SRC_DIR = os.path.realpath(os.path.join(cur_dir, "..", "urh", "build", "lib.linux-x86_64-3.8"))
+
+if os.path.isdir(SRC_DIR):
+    sys.path.insert(0, SRC_DIR)
+
+from urh.signalprocessing.IQArray import IQArray
+from urh.util import util
+
+util.set_shared_library_path()
+
+from urh.util import Logger
+from urh.util.Logger import logger
+
+from urh.cli import urh_cli
+from urh.dev.native.lib import hackrf
+from multiprocessing import Array
+
+
+
+class SendConfig(object):
+    def __init__(self, send_buffer, total_samples: int):
+        self.send_buffer = send_buffer
+        self.total_samples = total_samples
+        self.current_sent_index = 0
+
+    def get_data_to_send(self, buffer_length: int):
+        try:
+            if self.sending_is_finished():
+                return np.zeros(1, dtype=self.send_buffer._type_._type_)
+
+            index = self.current_sent_index
+            np_view = np.frombuffer(self.send_buffer, dtype=self.send_buffer._type_._type_)
+            result = np_view[index:index + buffer_length]
+
+            self.progress_send_status(len(result))
+            return result
+        except (BrokenPipeError, EOFError):
+            return np.zeros(1, dtype=self.send_buffer._type_._type_)
+
+    def sending_is_finished(self):
+        return self.current_sent_index >= self.total_samples
+
+    def progress_send_status(self, buffer_length: int):
+        self.current_sent_index += buffer_length
+        if self.current_sent_index >= self.total_samples - 1:
+            self.current_sent_index = self.total_samples
+
+class Sender:
+
+    def __init__(self, arguments: argparse.Namespace):
+        hackrf.setup(None)
+        self.reset(arguments)
+        
+    def reset(self, arguments: argparse.Namespace):
+    
+        bandwidth = arguments.sample_rate if arguments.bandwidth is None else arguments.bandwidth
+        gain = 20 if arguments.gain is None else arguments.gain
+        if_gain = 20 if arguments.if_gain is None else arguments.if_gain
+
+        hackrf.set_freq(arguments.frequency)
+        hackrf.set_sample_rate(arguments.sample_rate)
+        hackrf.set_baseband_filter_bandwidth(bandwidth)
+        hackrf.set_rf_gain(gain)
+        hackrf.set_if_tx_gain(if_gain)
+
+    @staticmethod
+    def iq_to_bytes(samples: np.ndarray):
+        arr = Array("B", 2 * len(samples), lock=False)
+        numpy_view = np.frombuffer(arr, dtype=np.uint8)
+        numpy_view[:] = samples.flatten(order="C")
+        return arr
+
+    def init_send_parameters(self, samples_to_send: IQArray):
+        samples_to_send_ = samples_to_send.convert_to(np.int8)
+        send_buffer = self.iq_to_bytes(samples_to_send_)
+        total_samples = len(send_buffer)
+        return SendConfig(send_buffer, total_samples)
+
+    def send(self, samples_to_send: np.ndarray):
+        send_config = self.init_send_parameters(samples_to_send)
+
+        ret = hackrf.start_tx_mode(send_config.get_data_to_send)
+        if ret != 0:
+            print("ERROR: enter_async_send_mode failed")
+            return False
+
+        while not send_config.sending_is_finished():
+            try:
+                time.sleep(0.01)
+            except KeyboardInterrupt:
+                pass
+
+        hackrf.stop_tx_mode()
+
+
+    def shutdown_device(self):
+        hackrf.close()
+        hackrf.exit()
+
+
+
+def main(messages):
+
+    DEFAULT_CARRIER_AMPLITUDE = 1
+    DEFAULT_CARRIER_PHASE = 0
+    DEFAULT_NOISE = 0.1
+    DEFAULT_CENTER = 0
+    DEFAULT_CENTER_SPACING = 0.1
+    DEFAULT_TOLERANCE = 5
+
+    args = argparse.Namespace()
+    args.receive = False
+    args.transmit = True
+    args.filename = None
+    args.encoding = None
+    args.hex = False
+    args.pause = 262924
+    args.gain = None
+    args.if_gain = None
+    args.bandwidth = None
+    args.baseband_gain = None
+    args.modulation_type = "FSK"
+    args.samples_per_symbol = 3100
+    args.center = DEFAULT_CENTER
+    args.center_spacing = DEFAULT_CENTER_SPACING
+    args.noise = DEFAULT_NOISE
+    args.tolerance = DEFAULT_TOLERANCE
+    args.bits_per_symbol = 1
+    args.carrier_frequency = 27.1e6
+    args.carrier_amplitude = DEFAULT_CARRIER_AMPLITUDE
+    args.carrier_phase = DEFAULT_CARRIER_PHASE
+    args.parameters = [92e3, 95e3]
+    args.device_backend = "native"
+    args.device = "HackRF"
+    args.device_identifier = None
+    args.frequency = 27.1e6
+    args.frequency_correction = 1
+    args.sample_rate = 2e6 
+    args.messages = messages
+    args.raw = False
+
+    logger.setLevel(logging.ERROR)
+    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
+    Logger.save_log_level()
+
+    modulator = urh_cli.build_modulator_from_args(args)
+    messages_to_send = urh_cli.read_messages_to_send(args)
+    samples_to_send = urh_cli.modulate_messages(messages_to_send, modulator)
+    
+    sender = Sender(args)
+
+    try:
+        sender.send(samples_to_send)
+        time.sleep(1)
+    
+        sender.reset(args)
+        sender.send(samples_to_send)
+    
+    finally:    
+        sender.shutdown_device()
