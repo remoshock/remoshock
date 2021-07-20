cf8f95775829b8d4554030b1b142af9d5022c9c6
Nils Winter <nils.winter@yahoo.com>
2021-07-20 21:36:12 +0200

    implemented command line interface

---
+++ src/pyshock/cli.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+#
+# Copyright nilswinter 2020-2021. License: AGPL
+#_______________________________________________
+
+
+import argparse
+import sys
+
+from pyshock.core.pyshock import Pyshock, PyshockMock
+from pyshock.core.action import Action
+
+class PyshockCli:
+
+    def __parse_args(self):
+        parser = argparse.ArgumentParser(description="Shock collar remote",
+                                         epilog="Please see https://github.com/pyshock/pyshock for documentation.")
+        parser.add_argument("-r", "--receiver",
+                            type=int,
+                            default=0,
+                            metavar="n",
+                            help="index of receiver entry from pyshock.ini, starting at 0")
+        parser.add_argument("-a", "--action", 
+                            default="BEEP",
+                            choices=["LED", "BEEP", "VIB", "ZAP", "BEEPZAP"], 
+                            help="Action to perform")
+        parser.add_argument("-d", "--duration", 
+                            type=int,
+                            default=250,
+                            metavar="n",
+                            help="duration in ms  (Note: PAC uses an impulse duration of 250ms)")
+        parser.add_argument("-p", "--power",
+                            type=int,
+                            default=0,
+                            metavar="n",
+                            help="power level (0-100)")
+        parser.add_argument("--mock",
+                            action="store_true",
+                            help=argparse.SUPPRESS)
+        parser.add_argument("-v", "--verbose",
+                            action="store_true",
+                            help="prints debug messages")
+        parser.add_argument("--version",
+                            action="version",
+                            version="0.1")
+    
+        self.args = parser.parse_args()
+        print("Command: " + sys.argv[0] + " --receiver " + str(self.args.receiver) + " --action " + self.args.action
+              + " --duration " + str(self.args.duration) + " --power " + str(self.args.power))
+
+
+    def __boot_pyshock(self):
+        if self.args.mock:
+            self.pyshock = PyshockMock(self.args)
+        else:
+            self.pyshock = Pyshock(self.args)
+        self.pyshock.boot()
+
+
+    def __process_action(self):
+        action = Action[self.args.action]
+        self.pyshock.command(action, self.args.receiver, self.args.power, self.args.duration)
+
+    
+    def start(self):
+        self.__parse_args()
+        self.__boot_pyshock()
+        self.__process_action()
+
---
+++ src/pyshockcli.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+#
+# Copyright nilswinter 2020-2021. License: AGPL
+#_______________________________________________
+
+from pyshock.cli import PyshockCli
+
+PyshockCli().start() 
