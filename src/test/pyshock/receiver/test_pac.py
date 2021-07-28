eff5533d5f8a6723b6247d797e5cc8e5aeefd612
Nils Winter <nils.winter@yahoo.com>
2021-07-29 00:27:36 +0200

    use builtin assertEqual in tests

--- src/test/pyshock/receiver/test_pac.py
+++ src/test/pyshock/receiver/test_pac.py
@@ -5,32 +5,22 @@ from pyshock.receiver.pac import Pac
 
 class PacTestCase(unittest.TestCase):
 
-
-    def assertThat(self, message, expected, actual):
-        if expected == actual:
-            print(message + ": pass")
-        else:
-            print(message + ": FAILED")
-            print("   Expected: " + expected)
-            print("   Actual  : " + actual)
-
-
     def test_encoding(self):
         pacdog = Pac("PAC1", "#FFF", "010110110", 1)
         expected = "010101010101010111110010110010110010010110010010010110110010110110010010010010010010110110010"
         data = "010100100011011000000110"
         encoded = pacdog.encode_for_transmission(data)
-        self.assertThat("encoding", expected, encoded)
+        self.assertEqual(expected, encoded, "encoding")
 
 
     def test_generate(self):
         pacdog = Pac("PAC1", "#FFF", "010110110", 1)
         expected = "010100100011011000000110"
         generated = pacdog.generate("010110110", 18, 2, 1)
-        self.assertThat("generation", expected, generated)
+        self.assertEqual(expected, generated, "generation")
 
 
     def test_calculate_intensity_code(self):
         pacdog = Pac("PAC1", "#FFF", "010110110", 1)
-        self.assertThat("intensity  1", "100000", pacdog.calculate_intensity_code(1))
-        self.assertThat("intensity 32", "000001", pacdog.calculate_intensity_code(32))
+        self.assertEqual("100000", pacdog.calculate_intensity_code(1), "intensity  1")
+        self.assertEqual("000001", pacdog.calculate_intensity_code(32), "intensity 32")
