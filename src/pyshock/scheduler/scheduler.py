e784da7768af011a31f23c7ed6358cb1fa4654ec
Nils Winter <nils.winter@yahoo.com>
2021-08-02 18:37:44 +0200

    implemented a simple scheduler

--- src/pyshock/scheduler/scheduler.py
+++ src/pyshock/scheduler/scheduler.py
@@ -2,53 +2,95 @@
 # Copyright nilswinter 2021. License: AGPL
 # ________________________________________
 
+
+import datetime
 import threading
-
-from pyshock.scheduler.task import Task
-
-
-def task_indexer(task):
-    return task.timestamp
 
 
 class Scheduler:
+    """a scheduler for future tasks"""
 
     def __init__(self):
-        self.scheduled_tasks = []
+        self.__scheduled_tasks = {}
+        self.__scheduled_task_references = {}
+        self.__scheduled_groups = {}
         self.lock = threading.RLock()
 
 
-    def add(self, tasks):
+    def schedule_task(self, task):
+        """schedules a tasks for future processing
+
+        @param task a task object
+        @return true, if the task was scheduled, false otherwise
+                (e. g. because timestamp was in the past"""
+
         with self.lock:
-            if isinstance(tasks, list):
-                for task in tasks:
-                    self.scheduled_tasks.append(task)
-            elif isinstance(tasks, Task):
-                self.scheduled_tasks.append(tasks)
-            else:
-                raise Exception("Invalid type of parameter: " + str(tasks))
+            wait_time = (task.timestamp-datetime.datetime.now()).total_seconds()
+            if wait_time < 0:
+                return False
 
-            self.scheduled_tasks.sort(task_indexer)
+            timer_reference = threading.Timer(wait_time, task)
+            self.__scheduled_tasks[task.identifier] = task
+            self.__scheduled_task_references[task.identifier] = timer_reference
+            if task.group is not None:
+                if task.group in self.__scheduled_groups:
+                    group = self.__scheduled_groups[task.group]
+                else:
+                    group = {}
+                    self.__scheduled_groups[task.group] = group
+                group.append(task.identifer)
 
+            timer_reference.start()
+            return True
 
-    def remove(self, identifier):
+    def internal_cleanup_task(self, task):
+        """cleansup datastructure after tasks was executed or canceled.
+        Do not call directly.
+
+        @param task the task to cleanup"""
+
         with self.lock:
-            for i in range(len(self.scheduled_tasks) - 1, -1, -1):
-                if self.scheduled_tasks[i].identifier == identifier:
-                    del self.scheduled_tasks[i]
+            self.__scheduled_tasks.pop(task.identifier, None)
+            self.__scheduled_task_references.pop(task.identifier, None)
+
+            if task.group is not None:
+                if task.group in self.__scheduled_groups:
+                    group = self.__scheduled_groups[task.group]
+                    group.pop(task.identifier, None)
+                    if len(group) == 0:
+                        self.__scheduled_groups.pop(task.group, None)
 
 
-    def remove_group(self, group):
+    def cancel_task(self, identifer):
+        """chancels a future task
+
+        @param identifier identifier of this task"""
+
         with self.lock:
-            for i in range(len(self.scheduled_tasks) - 1, -1, -1):
-                if self.scheduled_tasks[i].group == group:
-                    del self.scheduled_tasks[i]
+            if identifer in self.__scheduled_tasks:
+                task = self.__scheduled_tasks.pop(identifer)
+                timer_reference = self.__scheduled_task_references.pop(identifer, None)
+                if timer_reference is not None:
+                    timer_reference.cancel()
+                self.internal_cleanup_task(task)
 
 
-#    def start(self):
-#        x = threading.Thread(target=thread_function, args=(1,), daemon=True)
+    def cancel_group(self, group_identifier):
+        """chancels all future tasks belonging to the group
 
-#        t = threading.Timer(30.0, my_function)
-#        keep dict with identifer and timer-reference, group-identifer and list of timer references
-#        remove from dicts, from event handler
-#
+        @param group_identifier identifier of this group"""
+        with self.lock:
+            if group_identifier in self.__scheduled_groups:
+                task_identifiers = self.__scheduled_groups.pop(group_identifier)
+                for task_identifier in task_identifiers:
+                    self.cancel_task(task_identifier)
+
+
+__scheduler = None
+
+def scheduler():
+    """a scheduler for future tasks"""
+    # singleton pattern
+    if __scheduler is None:
+        __scheduler = Scheduler()
+    return __scheduler
--- src/pyshock/scheduler/task.py
+++ src/pyshock/scheduler/task.py
@@ -7,10 +7,21 @@ class Task:
     """A task that can be scheduled"""
 
 
-    def __init__(self, timestamp, identifier, group, receiver, action, power, duration):
+    def __init__(self, timestamp, identifier, group_identifier, receiver, action, power, duration):
+        """a schedulable task object
+
+        @param timestamp when this task should be executed
+        @param identifier a unique identifier, which may be used to cancel the task before it is executed
+        @param group_identifier an identifier, which may be used to cannel all task with same same group identifier
+        @param receiver index of pyshock receiver
+        @param action Action (e. g. BEEP, SHOCK)
+        @param power power level (0-100)
+        @param duration duration in ms
+        """
+
         self.timestamp = timestamp
         self.identifier = identifier
-        self.group = group
+        self.group_identifier = group_identifier
         self.receiver = receiver
         self.action = action
         self.power = power
