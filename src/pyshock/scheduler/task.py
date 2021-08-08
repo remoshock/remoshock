05239e19606210e4e9c683046dd4257df10727cf
Nils Winter <nils.winter@yahoo.com>
2021-08-08 07:26:58 +0200

    create random identifier, if none is provided

--- src/pyshock/scheduler/task.py
+++ src/pyshock/scheduler/task.py
@@ -2,14 +2,15 @@
 # Copyright nilswinter 2021. License: AGPL
 # ________________________________________
 
-
+import secrets
+import string
 from pyshock.scheduler.scheduler import scheduler
 
 
 class Task:
     """A task that can be scheduled"""
 
-    def __init__(self, timestamp, identifier, group_identifier):
+    def __init__(self, timestamp, identifier=None, group_identifier=None):
         """a schedulable task object
 
         @param timestamp when this task should be executed
@@ -18,6 +19,9 @@ class Task:
         """
         self.timestamp = timestamp
         self.identifier = identifier
+        if identifier is None:
+            charset = string.ascii_lowercase + string.ascii_uppercase + string.digits
+            self.identifier = ''.join(secrets.choice(charset) for _ in range(40))
         self.group_identifier = group_identifier
 
 
