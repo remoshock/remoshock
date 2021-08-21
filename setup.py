f610e5e5728108f52b971346b5cc84c046cbde84
Nils Winter <nils.winter@yahoo.com>
2021-08-22 01:10:44 +0200

    setup.py for pip

--- setup.py
+++ setup.py
@@ -8,21 +8,12 @@ setuptools.setup(
     version = '0.5.0',
     author = 'Nils Winter',
     author_email = 'nils.winter@yahoo.com',
-    description = 'Computer based shock-collar remote control with "evil" features.',
-    long_description = long_description,
-    long_description_content_type = 'text/markdown',
-    url = 'https://github.com/pypa/sampleproject',
     classifiers = [
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: GNU Affero General Public License v3',
         'Operating System :: POSIX :: Linux',
     ],
-    package_dir={'': 'src'},
-    packages=setuptools.find_namespace_packages(where='src'),
-    python_requires = '>=3.6',
-    package_data={
-      '': ['*'],
-    },
+    description = 'Computer based shock-collar remote control with "evil" features.',
     entry_points={
         'console_scripts': [
             'remoshockcli = remoshock.cli:main',
@@ -30,4 +21,16 @@ setuptools.setup(
             'remoshockserver = remoshock.server:main'
         ],
     },
+#    install_requires=['numpy'],
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    license="GNU Affero General Public License v3 (AGPL)",
+    license_files = ('docs/LICENSE.md'),
+    package_data={
+      '': ['*'],
+    },
+    package_dir={'': 'src'},
+    packages=setuptools.find_namespace_packages(where='src'),
+    python_requires = '>=3.6',
+    url = 'https://github.com/remoshock/remoshock',
 )
